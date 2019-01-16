7ddf86fc0605a973dd9437d572c65181003deec3
Cassandra Romero <romero.cassandra1@gmail.com>
2019-01-16 12:03:19 -0700

    added php files and content to kristenShepard.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pure-Manage
+ * Date: 1/16/2019
+ * Time: 11:48 AM
+ */
---
+++ kristenShepard.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Kristen Shepard Presona</title>
+	</head>
+	<body>
+		<strong>Name:</strong> Kristen Shepard
+		<br>
+		<strong>Age:</strong> 21
+		<br>
+		<strong>Profession:</strong> College Student
+		<br>
+		<strong>Hobbies:</strong> Spending time with sorority sisters, attending college football games, marathon running
+	</body>
+</html>
