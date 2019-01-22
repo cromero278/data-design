d353667d0f852593396dbe60fa6ff4a9c52fc898
Cassandra Romero <romero.cassandra1@gmail.com>
2019-01-22 10:30:54 -0700

    updates to oml.sql

--- sql/oml.sql
+++ sql/oml.sql
@@ -11,3 +11,6 @@ DELETE FROM user WHERE userId = UNHEX("1402800157c14daaaa3674f08f51235b");
 INSERT INTO listing (listingId, listingCategoryId, listingUserId, listingBrand, listingDescripton, listingName, listingPhotoUrl, listingPrice) VALUES (UNHEX("a19c51d2fb9a402e83e2745e24a41005"), UNHEX("2df2730320e2453ebb9d123cbc072ffa"), UNHEX("44bef377e68644629c1b28cf2ad8242a"), "Frye", "New with tags brown purse", "Frye Purse", "https://poshmark.com/fryepurse/1234", "124.99");
 
 SELECT userFirstName, userLastName, userEmail FROM user WHERE userId = UNHEX("44bef377e68644629c1b28cf2ad8242a");
+
+SELECT listing.listingId, listing.listingDescripton, listing.listingName FROM listing INNER JOIN user
+ON listing.listingUserId = user.userId WHERE listingBrand = "Frye";
